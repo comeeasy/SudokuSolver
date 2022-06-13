be3d4455424cc3116914dee87d892b4dae3ced04
comeeasy <zz1236zz@naver.com>
2022-06-14 00:13:22 +0900

    Stop..

--- sudokuSolver/sudokusolver/asgi.py
+++ sudokuSolver/sudokusolver/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sudokuSolver project.
+ASGI config for sudokusolver project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudokuSolver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudokusolver.settings')
 
 application = get_asgi_application()
--- sudokuSolver/sudokusolver/urls.py
+++ sudokuSolver/sudokusolver/urls.py
@@ -1,4 +1,4 @@
-"""sudokuSolver URL Configuration
+"""sudokusolver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
