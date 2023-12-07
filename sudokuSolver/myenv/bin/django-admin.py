dc8090ebf8dd1ba9b5d8b157740312919ab50c22
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2023-12-08 04:02:22 +0900

    설정 변경
    스도쿠 문제풀이 가능

---
+++ sudokuSolver/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/baejongchan/Documents/GitHub/SudokuSolver/sudokuSolver/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
